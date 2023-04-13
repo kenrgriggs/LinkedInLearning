30f7fba4aff6ff57ae5c33c083fdacb28aa034ce
Kenneth Griggs <kenrgriggs@gmail.com>
2023-04-13 18:52:47 -0400

    added comments to plan structure

---
+++ LinkedInLearningIsEasy.py
@@ -0,0 +1,14 @@
+
+# Load in required modules
+
+# Define variables
+
+# Open web browser (new tab)
+
+# Navigate to website
+
+# Login to website
+
+# Search for random learning path
+
+# Play full learning path
